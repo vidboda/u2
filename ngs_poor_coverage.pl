da2cfe6b33ef4b4d6166c70189397a83e43038fa
hg <david.baux@inserm.fr>
2018-01-09 11:56:13 +0100

    Initial commit

---
+++ ngs_poor_coverage.pl
@@ -0,0 +1,125 @@
+BEGIN {delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV', 'PATH'};}
+
+use strict;
+#use CGI;
+#use DBI;
+#use AppConfig qw(:expand :argcount);
+use U2_modules::U2_users_1;
+use U2_modules::U2_init_1;
+use U2_modules::U2_subs_1;
+use U2_modules::U2_subs_2;
+
+#    This program is part of ushvam2, USHer VAriant Manager version 2
+#    Copyright (C) 2012-2016  David Baux
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#		Home page of U2
+
+
+##Basic init of USHVaM 2 perl scripts
+#	env variables
+#	get config infos
+#	initialize DB connection
+#	initialize HTML (change page title if needed, as well as CSS files and JS)
+#	Load standard JS, CSS and fixed html
+#	identify users
+#	just copy at the beginning of each script
+
+$CGI::POST_MAX = 1024; #* 100;  # max 1K posts
+$CGI::DISABLE_UPLOADS = 1;
+
+
+
+my $config_file = U2_modules::U2_init_1->getConfFile();
+my $config = U2_modules::U2_init_1->initConfig();
+$config->file($config_file);# or die $!;
+my $DB = $config->DB();
+my $HOST = $config->HOST();
+my $DB_USER = $config->DB_USER();
+my $DB_PASSWORD = $config->DB_PASSWORD();
+my $CSS_PATH = $config->CSS_PATH();
+my $CSS_DEFAULT = $config->CSS_DEFAULT();
+my $JS_PATH = $config->JS_PATH();
+my $JS_DEFAULT = $config->JS_DEFAULT();
+my $HTDOCS_PATH = $config->HTDOCS_PATH();
+
+my @styles = ($CSS_PATH.'font-awesome.min.css', $CSS_PATH.'w3.css', $CSS_DEFAULT, $CSS_PATH.'fullsize/fullsize.css', $CSS_PATH.'jquery.alerts.css', $CSS_PATH.'datatables.min.css');
+
+my $q = new CGI;
+
+my $dbh = DBI->connect(    "DBI:Pg:database=$DB;host=$HOST;",
+                        $DB_USER,
+                        $DB_PASSWORD,
+                        {'RaiseError' => 1}
+                ) or die $DBI::errstr;
+
+
+
+print $q->header(-type => 'text/html', -'cache-control' => 'no-cache'),
+	$q->start_html(-title=>"USH non-USH",
+                        -lang => 'en',
+                        -style => {-src => \@styles},
+                        -head => [
+				$q->Link({-rel => 'icon',
+					-type => 'image/gif',
+					-href => $HTDOCS_PATH.'data/img/animated_favicon1.gif'}),
+				$q->Link({-rel => 'search',
+					-type => 'application/opensearchdescription+xml',
+					-title => 'U2 search engine',
+					-href => $HTDOCS_PATH.'u2browserengine.xml'}),
+				$q->meta({-http_equiv => 'Cache-control',
+					-content => 'no-cache'}),
+				$q->meta({-http_equiv => 'Pragma',
+					-content => 'no-cache'}),
+				$q->meta({-http_equiv => 'Expires',
+					-content => '0'})],
+                        -script => [{-language => 'javascript',
+                                -src => $JS_PATH.'jquery-1.7.2.min.js', 'defer' => 'defer'},
+                                {-language => 'javascript',
+                                -src => $JS_PATH.'jquery.fullsize.pack.js', 'defer' => 'defer'},
+				{-language => 'javascript',
+                                -src => $JS_PATH.'jquery.validate.min.js', 'defer' => 'defer'},
+				{-language => 'javascript',
+				-src => $JS_PATH.'jquery.alerts.js', 'defer' => 'defer'},
+				{-language => 'javascript',
+				-src => $JS_PATH.'datatables.min.js', 'defer' => 'defer'},
+                                {-language => 'javascript',
+                                -src => $JS_PATH.'jquery.autocomplete.min.js', 'defer' => 'defer'},
+                                {-language => 'javascript',
+                                -src => $JS_DEFAULT}],		
+                        -encoding => 'ISO-8859-1', 'defer' => 'defer');
+
+my $user = U2_modules::U2_users_1->new();
+
+
+U2_modules::U2_subs_1::standard_begin_html($q, $user->getName(), $dbh);
+
+##end of Basic init
+
+
+
+
+
+
+##Basic end of USHVaM 2 perl scripts:
+
+U2_modules::U2_subs_1::standard_end_html($q);
+
+print $q->end_html();
+
+exit();
+
+##End of Basic end
