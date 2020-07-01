c165eb73f95c8a368fc9f1d57371c9b34c080e66
hg <david.baux@inserm.fr>
2020-07-01 12:02:47 +0200

    fixed bug with ucsc url

--- U2_modules/U2_subs_3.pm
+++ U2_modules/U2_subs_3.pm
@@ -1522,11 +1522,11 @@ sub create_variant_vv {
 		my $client = REST::Client->new();
 		# print "http://togows.org/api/ucsc/hg19/$chr:$x-$y<br/>";
 		# exit;
-		$client->getUseragent()->ssl_opts(verify_hostname => 0);
-		$client->getUseragent()->ssl_opts(SSL_verify_mode => 'SSL_VERIFY_NONE');
+		# $client->getUseragent()->ssl_opts(verify_hostname => 0);
+		# $client->getUseragent()->ssl_opts(SSL_verify_mode => 'SSL_VERIFY_NONE');
 		$client->GET("https://genome-euro.ucsc.edu/cgi-bin/hubApi/getData/sequence?genome=hg19;chrom=chr$chr;start=$x;end=$y");
-		print STDERR $client;
-		print STDERR "https://genome-euro.ucsc.edu/cgi-bin/hubApi/getData/sequence?genome=hg19;chrom=chr$chr;start=$x;end=$y\n";
+		# print STDERR $client;
+		# print STDERR "https://genome-euro.ucsc.edu/cgi-bin/hubApi/getData/sequence?genome=hg19;chrom=chr$chr;start=$x;end=$y\n";
 		# print STDERR $client->responseContent()."\n";
 		my $ucsc_response = decode_json($client->responseContent());
 		# $client->GET("http://togows.org/api/ucsc/hg19/chr$chr:$x-$y");
