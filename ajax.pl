f8ee5f5e5f8279a74fe46c97cc69eb572cd7b04c
beboche <david.baux@inserm.fr>
2021-10-20 14:32:17 +0200

    added 149 exp to run_table

--- ajax.pl
+++ ajax.pl
@@ -1264,7 +1264,7 @@ if ($q->param('run_table') && $q->param('run_table') == 1) {
 	else {$query = "SELECT DISTINCT(a.run_id), a.type_analyse, b.filtering_possibility FROM miseq_analysis a, valid_type_analyse b WHERE a.type_analyse = b.type_analyse AND b.type_analyse  = '$analysis' ORDER BY a.type_analyse DESC, a.run_id;"}
 	#my $dates = "\"date\": [
 	#";
-	my $i = my $j = my $k = my $l = my $m = my $n = my $o = my $p = my $r = my $s = my $t = my $u = my $v = 0;
+	my $i = my $j = my $k = my $l = my $m = my $n = my $o = my $p = my $r = my $s = my $t = my $u = my $v = my $w = 0;
 	my $sth = $dbh->prepare($query);
 	my $res = $sth->execute();
 	if ($res ne '0E0') {
@@ -1304,6 +1304,7 @@ if ($q->param('run_table') && $q->param('run_table') == 1) {
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-132') {$n++;}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-152') {$t++;}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-158') {$v++;}
+      elsif ($result->{'type_analyse'} eq 'MiniSeq-149') {$w++;}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-3') {$p++;}
 			elsif ($result->{'type_analyse'} eq 'NextSeq-ClinicalExome') {$r++;}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-2') {$s++;}
@@ -1349,6 +1350,7 @@ if ($q->param('run_table') && $q->param('run_table') == 1) {
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-132') {$content .= $q->td("Run $n")}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-152') {$content .= $q->td("Run $t")}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-158') {$content .= $q->td("Run $v")}
+      elsif ($result->{'type_analyse'} eq 'MiniSeq-149') {$content .= $q->td("Run $w")}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-3') {$content .= $q->td("Run $p")}
 			elsif ($result->{'type_analyse'} eq 'NextSeq-ClinicalExome') {$content .= $q->td("Run $r")}
 			elsif ($result->{'type_analyse'} eq 'MiniSeq-2') {$content .= $q->td("Run $s")}
