cfd1fe171b1d4d6d2aba2eb9cdeb9d9e676a8dc8
beboche <david.baux@inserm.fr>
2022-03-30 10:52:22 +0200

    updated covreport links

--- ajax.pl
+++ ajax.pl
@@ -1696,7 +1696,7 @@ if ($q->param('asked') && $q->param('asked') eq 'covreport') {
 		if (-e $ABSOLUTE_HTDOCS_PATH."CovReport/CovReport/pdf-results/".$id.$number."-".$analysis."-".$filter."_coverage.pdf") {
 			# print $q->start_span().$q->a({ 'href' => $HTDOCS_PATH."CovReport/CovReport/pdf-results/".$id.$number."-".$analysis."-".$filter."_coverage.pdf", 'target' => '_blank'}, 'Download CovReport').$q->end_span();
       print $q->start_span().$q->a({ 'href' => $HTDOCS_PATH."DS_data/covreport/".$id.$number."/".$id.$number."-".$analysis."-".$filter."_coverage.pdf", 'target' => '_blank'}, 'Download CovReport').$q->end_span();
-			U2_modules::U2_subs_2::send_general_mail($user, "CovReport ready for $id$number-$analysis-$filter", "Hi ".$user->getName().",\nYou can download the CovReport file here:\n$HOME_IP/ushvam2/DS_data/covreport/$id$number/$id$number-$analysis-".$filter."_coverage.pdf\n");
+			U2_modules::U2_subs_2::send_general_mail($user, "CovReport ready for $id$number-$analysis-$filter", "Hi ".$user->getName().",\nYou can download the CovReport file here:\n".$HOME_IP."ushvam2/DS_data/covreport/$id$number/$id$number-$analysis-".$filter."_coverage.pdf\n");
 			# attempt to trigger autoFS
 			open HANDLE, ">>".$ABSOLUTE_HTDOCS_PATH."DS_data/covreport/touch.txt";
 			sleep 3;
