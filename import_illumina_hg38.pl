1b708266541618b02eac0714a3bb171c830cc07a
beboche <david.baux@inserm.fr>
2024-06-12 17:32:43 +0200

    added vaf filter

--- import_illumina_hg38.pl
+++ import_illumina_hg38.pl
@@ -306,6 +306,9 @@ if ($step && $step == 2) {
 
 				# if ($var_chr eq 'chr4' && $var_pos > 15997568) {exit 0}
 
+				# filter out var_vf > 0.15
+				if ($var_vf > 0.15) {$message .= "$id$number: WARNING $var_chr-$var_pos-$var_ref-$var_alt filtered out with VAF: $var_vf\n";next VCF;}
+
 				# we check wether the variant is in our genes or not
 				# we just query ushvam2
 				if  ($var_chr =~ /^chr($U2_modules::U2_subs_1::CHR_REGEXP)$/o) {$var_chr = $1}
